4ffda6a10949cc332fa24807b88b7ec26b63538a
Barre Nils Fredrik Svante Åke Blomberg <fred03.blomberg@gmail.com>
2021-01-21 11:04:01 +0100

    Initialize new project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextBaseratSpel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
